cc314f37591340ce0680f36fcf4629fdadc8c9f0
Shamon S <shamonsha665@gmail.com>
2016-03-30 12:52:08 +0530

    Added files via upload
    Added java script bridge file and documentation.

---
+++ gpsDetectionPlugin.js
@@ -0,0 +1,14 @@
+cordova.define('cordova/plugin/gpsDetectionPlugin', function(require, exports, module) {
+    var exec = require('cordova/exec');
+
+    var gpsDetect = function() {};
+
+    gpsDetect.prototype.checkGPS = function(successCallback, failureCallback) {
+    	exec(successCallback, failureCallback, 'GpsDetectionPlugin', 'gpsDetection', []);
+    };
+    gpsDetect.prototype.switchToLocationSettings = function(successCallback, failureCallback) {
+    	exec(successCallback, failureCallback, 'GpsDetectionPlugin', 'switchToLocationSettings', []);
+    };
+    var gpsDetect = new gpsDetect();
+    module.exports = gpsDetect;
+});    
